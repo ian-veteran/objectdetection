6811aeea58f8f19e92e8600822523f0ed7a9a6ec
ian-veteran <iankaranimutuma@gmail.com>
2024-07-14 13:08:36 +0300

    new_message

--- src/components/objectDetection/index.jsx
+++ src/components/objectDetection/index.jsx
@@ -3,6 +3,7 @@ import axios from "axios";
 import styled from "styled-components";
 import Spinner from "../Spinner";
 
+
 const ObjectDetectorContainer = styled.div`
   display: flex;
   flex-direction: column;
